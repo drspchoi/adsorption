4a5d7e2889b1c3cb57780d66fc1f5bf19adcf01e
drspchoi <drspchoi@gmail.com>
2024-08-15 16:18:45 -0700

    all kinetic models are combined into one file using for loop and *popt. Nth model needs to be rechecked

--- kinetic.py
+++ kinetic.py
@@ -1,66 +1,65 @@
 #%%
 import numpy as np
 from scipy.optimize import curve_fit
-import pylab
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
+#change it to reading CSV function and read data from csv files and transfrom it to np.array
 x=np.array([30,60,90,120,240,360,540,1440,2880,4320])
 y=np.array([66.31, 82.66, 91.09, 102.77, 127.80, 152.48, 179.30, 236.17,281.58,296.13])
 
-"""
-a=Qmax, b=k
-"""
+#Below is Nth model and utilize ODE to fit models. Initial guess didn't work
+def f(y,t,a,b):
+    return a*(b-y)**1; "a=k1, b=Qe, y=Qt,c=n" 
 
-def norder(time,a,b,c): 
-    n=time.size
-    p=np.zeros(n)
-    for i in range (n):
-        p[i]=a*(1-(1/(1+b*(c-1)*(a**(c-1))*time[i]))**(1/(c-1)))
-    return p
+def norder(t,a,b, y0):
+    """
+    solution to the ODE y'(t)=f(t,y,a,b) with initial condition y(0)=y0
+    """
+    norder=odeint(f, y0, t, args=(a,b))
+    return norder.ravel()
+
+#a=Qmax, b=k for below models
 def second(time,a,b):
-    n=time.size
-    p=np.zeros(n)
-    for i in range (n):
-        p[i]=b*a**2*time[i]/(1+a*b*time[i])
-    return p
-def first(time,a,b):
-    n=time.size
-    p=np.zeros(n)
-    for i in range (n):
-        p[i]=a*(1-np.exp(-b*time[i]))
-    return p
-def intra(time,a,b):
-    n=time.size
-    p=np.zeros(n)
-    for i in range (n):
-        p[i]=a*time[i]**0.5+b
-    return p
+    return b*a**2*time/(1+a*b*time)
 
-params=[400, 0.1, 2]
-popt,pcov=curve_fit(norder,x,y,p0=params)
-xdata=np.linspace(0,4320,400)
-yfit=norder(xdata,popt[0], popt[1], popt[2])
-pylab.plot(x,y,'.',xdata,yfit)
-print(popt[0], popt[1], popt[2])
-"""
-for equil in yfit:
-    print(equil)
-"""
-ysize=yfit.size
-xsize=xdata.size
-for i in range(ysize):
-    print(yfit[i])
-"""    
-for conc in xdata:
-    print(conc)
-"""
-"""
-for j in range(xsize):
-    print(xdata[j])
-"""    
-residuals=y-norder(x,popt[0],popt[1],popt[2])
-ss_res=np.sum(residuals**2)
-ss_tot=np.sum((y-np.mean(y))**2)
-r2=1-(ss_res/ss_tot)
-print(r2)
+def first(time,a,b):
+    return a*(1-np.exp(-b*time))
+
+def intra(time,a,b):
+    return a*time**0.5+b
+
+def r_square(x,y,model,popt):
+    residuals=y-model(x,*popt)
+    ss_res=np.sum(residuals**2)
+    ss_tot=np.sum((y-np.mean(y))**2)
+    r2=1-(ss_res/ss_tot)
+    return r2
+
+def graph(x,y,xdata,ydata,key, r_2):
+    plt.figure()
+    plt.title(key)
+    plt.plot(x,y,'o', label='data')
+    plt.plot(xdata,ydata,'r-', label='fit and r2: %5.3f' % r_2)
+    plt.legend()
+    plt.xlabel('time')
+    plt.ylabel('concentration')
+
+models={'First-order model': first, 'Second-order model': second, 
+        'Nth-order model': norder, 'Intra Diffusion model': intra}
+
+for key, model in models.items():
+    
+    if key=='Nth-order model':
+        params=[0.02,50,0.5]
+    else:
+        params=[0, 0.1]
+    
+    popt,pcov=curve_fit(model,x,y, p0=params)
+    xdata=np.linspace(0,4320,400)
+    ydata=model(xdata,*popt)
+    r_2=r_square(x,y,model,popt)
+    graph(x,y,xdata,ydata,key,r_2)
+
 
 # %%
