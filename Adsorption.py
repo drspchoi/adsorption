d978c916fb63089bfef0c3e52b9c4a81e95d7a24
drspchoi <drspchoi@gmail.com>
2024-09-25 14:32:55 -0700

    Uploading file logic changed. File had to be saved in server to run code. request.files check form has the field. So, to check whether the file was selected, request.files['file'] has to be checked.

--- Adsorption.py
+++ Adsorption.py
@@ -1,10 +1,12 @@
-
+import os
 from read_csv import readfile
 from adsorption_model import kinetic_models, isotherm_models
 from flask import Flask, render_template, request
 
 app=Flask(__name__)
- 
+folder='static' 
+app.config['upload']=folder
+
 @app.get('/')
 def index():
     return render_template('adsorption_plot.html')
@@ -12,23 +14,31 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
 
-    selection=[]
-    selection2=[]
     if 'file' not in request.files and 'file2' not in request.files:
 
         return "No file part"
+    
+    if request.files['file']=='' and request.files['file2']=='':
+        return "No file uploaded"
+    
+    selection=[]
+    selection2=[]
 
     if 'file' in request.files:
         file=request.files['file']
         if file.filename:
+            file_path=os.path.join(app.config['upload'], file.filename)
+            file.save(file_path)
+            print(file.filename)        
             x,y=readfile(file.filename)
             selection=request.form.getlist('Kinetic_model')
-            print(selection)
             kinetic_models(x,y,selection)
 
     if 'file2' in request.files:
         file2=request.files['file2']
-        if file2.filename:
+        if file2.filename !='':
+            file2_path=os.path.join(app.config['upload'], file2.filename)
+            file2.save(file2_path)             
             x,y=readfile(file2.filename)
             selection2=request.form.getlist('isotherm_model')
             isotherm_models(x,y,selection2)
